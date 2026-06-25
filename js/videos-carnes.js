9e33ff911fb06ab559bc95f771af4fd54d785e83
Tonycampo97 <anthonycampo97@outlook.com>
2026-06-25 08:58:14 -0500

    Modificacion modulo de carnes, se cargan los primeros videos con exito

--- js/videos-carnes.js
+++ js/videos-carnes.js
@@ -10,8 +10,45 @@
 const VIDEOS_SECCION = [
   {
     id: "carnes1",
-    title: "Video de ejemplo - carnes",
-    description: "Reemplaza este video por el contenido real de capacitación.",
-    youtubeId: "dQw4w9WgXcQ"
+    title: "Limpieza de Impresor en Bascula SM300H",
+    description: "Capacitación personal de carnes Limpieza de Impresor en Bascula SM300H",
+    youtubeId: "5YfWYNulk-w"
+  },
+  {
+    id: "carnes2",
+    title: "Limpieza de Impresor en Bascula SM100H",
+    description: "Capacitación personal de carnes Limpieza de Impresor en Bascula SM100H",
+    youtubeId: "jY074Gm6TG8"
+  },
+  {
+    id: "carnes3",
+    title: "BORRADO MEMEMORIA SM300",
+    description: "Proceso soporte tecnico BORRADO MEMEMORIA SM300",
+    youtubeId: "sbjaksx2KdU"
+  },
+  {
+    id: "carnes4",
+    title: "BORRAR BASE DE DATOS METTLER",
+    description: "Proceso tecnico BORRAR BASE DE DATOS METTLER",
+    youtubeId: "gq8gdAVIPTE"
+  },
+  {
+    id: "carnes5",
+    title: "AJUSTE SENSOR GAP BALANZA METTLER",
+    description: "Proceso tecnico AJUSTE SENSOR GAP BALANZA METTLER",
+    youtubeId: "y5hh2vZzIJk"
+  },
+  {
+    id: "carnes6",
+    title: "CALIBRACION INTENSIDAD CABEZAL",
+    description: "Proceso tecnico CALIBRACION INTENSIDAD CABEZAL",
+    youtubeId: "pOpRvGw_uRY"
+  },
+  {
+    id: "carnes7",
+    title: "AJUSTE SENSOR GAP SM300",
+    description: "Proceso tencico AJUSTE SENSOR GAP SM300",
+    youtubeId: "iNpBoIpx6Zs"
   }
+  
 ];
