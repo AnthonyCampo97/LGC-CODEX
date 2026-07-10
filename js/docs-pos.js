926456a3a55a6952705b3e5e9b06dd2d1596ab13
Tonycampo97 <anthonycampo97@outlook.com>
2026-07-10 12:49:28 -0500

    Se agrega instructivo novedades FE POS

--- js/docs-pos.js
+++ js/docs-pos.js
@@ -11,5 +11,11 @@ const DOCUMENTOS_SECCION = [
     title: "AUDITORIA Y REENVIO DE FACTURAS POS DIAN ",
     description: "Validacion de facturas POS ventas diarias",
     file: "docs/SG1/AUDITORIA-FEPOS.pdf"
+  },
+  {
+    id: "doc-pos2",
+    title: "INSTRUCTIVO DE COMUNICACIÓN EN NOVEDADES DE EMISIÓN DE FACTURACIÓN ELECTRONICA",
+    description: "Protocolo de actuación ante novedades en la emisión de facturación electrónica con clientes",
+    file: "docs/SG1/NOVEDADES-FE-POS.pdf"
   }
 ];
