e3a2aefc0eeee9205e4abcc4302e7729d0dc04c4
Tonycampo97 <anthonycampo97@outlook.com>
2026-07-30 15:37:36 -0500

    Se agrega video en modulo de carnes

--- js/videos-carnes.js
+++ js/videos-carnes.js
@@ -9,9 +9,15 @@
 
 const VIDEOS_SECCION = [
   {
-    id: "carnes8",
+    id: "carnes1",
     title: "REGISTRO INVIMA CARNES",
     description: "Agregar registro Invima en etiqueta de precios productos cárnicos desde el software administrativo ",
     youtubeId: "-z7s1YFoPNc"
+  },
+  {
+    id: "carnes2",
+    title: "CAMBIO DE ROLLO SM-300",
+    description: "Paso a paso cambio de rollo balanza carnes SM300",
+    youtubeId: "IlTvihLMYSE"
   }
 ];
